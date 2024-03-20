7c1c139e5973c81d3a8b98d745989b0e8c461fb4
Renard Ali <renardtnt1@gmail.com>
2024-03-19 21:06:17 -0400

    modified:   compareSortAlgorithms.c

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -30,34 +30,167 @@ size_t Size(void* ptr)
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+	// This build the heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+	{
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) 
+		{
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            // Recursively heapify the affected sub-tree
+            heapSort(arr, n);
+        }
+    }
 }
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+	if (l < r)
+	{
+        int m = l + (r - l) / 2;
+
+        // Recursively sort the first and second halves
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+
+        // Merge the sorted halves
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Allocate memory for temporary arrays
+        int *L = (int *)Alloc(n1 * sizeof(int));
+        int *R = (int *)Alloc(n2 * sizeof(int));
+
+        // Copy data to temporary arrays L[] and R[]
+        for (int i = 0; i < n1; i++)
+            L[i] = pData[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = pData[m + 1 + j];
+
+        // Merge the temporary arrays back into pData[l..r]
+        int i = 0; // Initial index of first subarray
+        int j = 0; // Initial index of second subarray
+        int k = l; // Initial index of merged subarray
+
+        while (i < n1 && j < n2)
+		{
+            if (L[i] <= R[j])
+			{
+                pData[k] = L[i];
+                i++;
+            }
+			
+			else 
+			{
+                pData[k] = R[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        // Copy the remaining elements of L[], if any
+        while (i < n1)
+		{
+            pData[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy the remaining elements of R[], if any
+        while (j < n2)
+		{
+            pData[k] = R[j];
+            j++;
+            k++;
+        }
+
+        // Deallocate memory for temporary arrays
+        DeAlloc(L);
+        DeAlloc(R);
+    }
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+	for (int i = 1; i < n; i++)
+	{
+        int key = pData[i];
+        int j = i - 1;
+
+        while (j >= 0 && pData[j] > key) 
+		{
+            pData[j + 1] = pData[j];
+            j--;
+        }
+
+        pData[j + 1] = key;
+    }
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+	int swapped;
+    do
+	{
+        swapped = 0;
+        for (int i = 0; i < n - 1; i++) 
+		{
+            if (pData[i] > pData[i + 1]) 
+			{
+                // Swap pData[i] and pData[i+1]
+                int temp = pData[i];
+                pData[i] = pData[i + 1];
+                pData[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+    } 
+	while (swapped);
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+	for (int i = 0; i < n - 1; i++)
+	{
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) 
+		{
+            if (pData[j] < pData[minIndex]) 
+			{
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) 
+		{
+            // Swap pData[i] and pData[minIndex]
+            int temp = pData[i];
+            pData[i] = pData[minIndex];
+            pData[minIndex] = temp;
+        }
+    }
 }
 
 // parses input file to an integer array
@@ -164,7 +297,7 @@ int main(void)
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
 		start = clock();
-		heapSort(pDataCopy, 0, dataSz - 1);
+		heapSort(pDataCopy, (dataSz - 1));
 		end = clock();
 		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
